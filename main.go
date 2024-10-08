6c68f6e0e065a4f9011f8327323ab26415ec89b2
Julie Krasnick <julie.krasnick@viam.com>
2024-10-08 12:01:15 -0400

    main is good

--- main.go
+++ main.go
@@ -9,18 +9,17 @@ import (
 	"go.viam.com/utils/rpc"
 )
 
-// type "go run main.go" to run this command
 func main() {
 	logger := logging.NewDebugLogger("client")
 	machine, err := client.New(
 		context.Background(),
-		"test1-main.myj4cbg09b.viam.cloud",
+		"ada-main.ikzfbcf5i3.viam.cloud",
 		logger,
 		client.WithDialOptions(rpc.WithEntityCredentials(
-			"d004e62e-cc0a-4ea4-8e13-59a8bc028eb0",
+			"783426e6-7f5c-4821-918e-0e9c9e00b99a",
 			rpc.Credentials{
 				Type:    rpc.CredentialsTypeAPIKey,
-				Payload: "ur6joo0p8j87yryjs5e1c5x41h5mmi2q",
+				Payload: "sbqyy0lvrxdtwrjaiml3f740kbq1qdij",
 			})),
 	)
 	if err != nil {
@@ -31,57 +30,19 @@ func main() {
 	logger.Info("Resources:")
 	logger.Info(machine.ResourceNames())
 
-	// camera-1
-	// camera1, err := camera.FromRobot(machine, "camera-1")
+	// Note that the pin supplied is a placeholder. Please change this to a valid pin.
+	// board-1
+	// board1, err := board.FromRobot(machine, "board-1")
 	// if err != nil {
-	// 	logger.Error(err)
-	// 	return
+	//   logger.Error(err)
+	//   return
 	// }
-	// camera1ReturnValue, err := camera1.Properties(context.Background())
+	// board1ReturnValue, err := board1.GPIOPinByName("16")
 	// if err != nil {
-	// 	logger.Error(err)
-	// 	return
+	//   logger.Error(err)
+	//   return
 	// }
-	// logger.Infof("camera-1 Properties return value: %+v", camera1ReturnValue)
-
-	// // camera-2
-	// camera2, err := camera.FromRobot(machine, "camera-2")
-	// if err != nil {
-	// 	logger.Error(err)
-	// 	return
-	// }
-	// camera2ReturnValue, err := camera2.Properties(context.Background())
-	// if err != nil {
-	// 	logger.Error(err)
-	// 	return
-	// }
-	// logger.Infof("camera-2 Properties return value: %+v", camera2ReturnValue)
-
-	// // youtube-stream
-	// youtubeStream, err := camera.FromRobot(machine, "youtube-stream")
-	// if err != nil {
-	// 	logger.Error(err)
-	// 	return
-	// }
-	// youtubeStreamReturnValue, err := youtubeStream.Properties(context.Background())
-	// if err != nil {
-	// 	logger.Error(err)
-	// 	return
-	// }
-	// logger.Infof("youtube-stream Properties return value: %+v", youtubeStreamReturnValue)
-
-	// // sensor-1
-	// sensor1, err := sensor.FromRobot(machine, "sensor-1")
-	// if err != nil {
-	// 	logger.Error(err)
-	// 	return
-	// }
-	// sensor1ReturnValue, err := sensor1.Readings(context.Background(), map[string]interface{}{})
-	// if err != nil {
-	// 	logger.Error(err)
-	// 	return
-	// }
-	// logger.Infof("sensor-1 Readings return value: %+v", sensor1ReturnValue)
+	// logger.Infof("board-1 GPIOPinByName return value: %+v", board1ReturnValue)
 
 	// camera-3
 	camera3, err := camera.FromRobot(machine, "camera-3")
@@ -97,5 +58,33 @@ func main() {
 	logger.Infof("camera-3 Properties return value: %+v", camera3ReturnValue)
 
 	_, _, err = camera3.Images(context.Background())
+
+	// camera2, err := camera.FromRobot(machine, "camera-2")
+	// if err != nil {
+	// 	logger.Error(err)
+	// 	return
+	// }
+	// camera2ReturnValue, err := camera2.Properties(context.Background())
+	// if err != nil {
+	// 	logger.Error(err)
+	// 	return
+	// }
+	// logger.Infof("camera-2 Properties return value: %+v", camera2ReturnValue)
+
+	// _, _, err = camera2.Images(context.Background())
+
+	// camera1, err := camera.FromRobot(machine, "camera-1")
+	// if err != nil {
+	// 	logger.Error(err)
+	// 	return
+	// }
+	// camera1ReturnValue, err := camera1.Properties(context.Background())
+	// if err != nil {
+	// 	logger.Error(err)
+	// 	return
+	// }
+	// logger.Infof("camera-1 Properties return value: %+v", camera1ReturnValue)
+
+	// _, _, err = camera1.Images(context.Background())
 
 }
